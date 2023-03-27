474c4501337c9f27af9db97d110436d458e5ca3c
mtarsitu <mario.tarsitu@gmail.com>
2023-03-27 21:42:46 +0300

    added logo

--- Front/src/global/navbars/DefaultNavbar/index.js
+++ Front/src/global/navbars/DefaultNavbar/index.js
@@ -1,19 +1,4 @@
 /* eslint-disable no-param-reassign */
-/**
-=========================================================
-* Material Kit 2 React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-kit-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { Fragment, useState, useEffect } from "react";
 
 // react-router components
@@ -551,7 +536,7 @@ function DefaultNavbar({ brand, routes, transparent, light, action, sticky, rela
 
 // Setting default values for the props of DefaultNavbar
 DefaultNavbar.defaultProps = {
-  brand: "Auto Completare Contracte",
+  brand: "AutoContract.ro",
   transparent: false,
   light: false,
   action: false,
--- Front/src/pages/Presentation/sections/Counters.js
+++ Front/src/pages/Presentation/sections/Counters.js
@@ -19,7 +19,7 @@ function Counters() {
               count={1}
               suffix=" min"
               title="Generare Contract"
-              description="De la pozele buletinelor și a cartii de identitate a mașinii până la un contract complet completat, procesul nostru de completare automată a contractelor este rapid și simplu"
+              description="De la pozele buletinelor și a cartii de identitate a mașinii până la un contract vanzare cumparare auto complet completat, procesul nostru de completare automată a contractelor este rapid și simplu"
             />
           </Grid>
           <Grid item xs={12} md={3} display="flex">
@@ -28,7 +28,7 @@ function Counters() {
               count={100}
               suffix="%"
               title="Securizat"
-              description="La AutoContracte.ro, protejarea informațiilor clienților noștri este o prioritate absolută. Nu reținem nicio informație furnizată de clienții noștri, nici măcar fotografiile încărcate pentru a completa contractele."
+              description="La AutoContracte.ro, protejarea informațiilor clienților noștri este o prioritate absolută. Nu reținem nicio informație furnizată de clienții noștri, nici măcar fotografiile încărcate pentru a completa contractul vanzare cumparare auto."
             />
             <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
           </Grid>
@@ -37,7 +37,7 @@ function Counters() {
               count={100}
               suffix="%"
               title="Model autentic"
-              description="Contractul de vânzare auto pe care îl veți primi de la AutoContracte.ro este un document recunoscut oficial atât de Primărie, cât și de Poliția Rutieră și Direcția Regim Permise de Conducere și Înmatriculare a Vehiculelor (DRPCIV)."
+              description="Contract vanzare cumparare auto pe care îl veți primi de la AutoContracte.ro este un document recunoscut oficial atât de Primărie, cât și de Poliția Rutieră și Direcția Regim Permise de Conducere și Înmatriculare a Vehiculelor (DRPCIV)."
             />
           </Grid>
           <Grid item xs={12} md={3}>
@@ -45,7 +45,7 @@ function Counters() {
               count={100}
               suffix="%"
               title="Confidentialitate"
-              description="Respectăm dreptul la confidențialitate al clienților noștri și asigurăm un mediu sigur și protejat pentru tranzacțiile lor. Puteți avea încredere că informațiile dvs. sunt în siguranță cu noi și că sunt utilizate numai în scopul de a completa contractul de vânzare auto."
+              description="Respectăm dreptul la confidențialitate al clienților noștri și asigurăm un mediu sigur și protejat pentru tranzacțiile lor. Puteți avea încredere că informațiile dvs. sunt în siguranță cu noi și că sunt utilizate numai în scopul de a completa in mod eficient un contract auto."
             />
           </Grid>
         </Grid>
